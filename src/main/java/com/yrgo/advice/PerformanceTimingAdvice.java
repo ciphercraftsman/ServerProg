bad7140fe7442c048e549d92880d95ee34d8d286
ciphercraftsman <fresjo0812@skola.goteborg.se>
2024-12-02 16:14:33 +0100

    fixed config for aspectj and point cut

--- src/main/java/com/yrgo/advice/PerformanceTimingAdvice.java
+++ src/main/java/com/yrgo/advice/PerformanceTimingAdvice.java
@@ -1,15 +1,9 @@
 package com.yrgo.advice;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
 public class PerformanceTimingAdvice {
 
-    @Around("execution(* com.yrgo.services..*.*(..)) || execution(* com.yrgo.dataaccess..*.*(..))")
     public Object measureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.nanoTime();
         Object result = joinPoint.proceed();
@@ -17,7 +11,7 @@ public class PerformanceTimingAdvice {
 
         String className = joinPoint.getTarget().getClass().getName();
         String methodName = joinPoint.getSignature().getName();
-        double timeTaken = (end - start) / 1_000_000.0; // Convert nanoseconds to milliseconds
+        double timeTaken = (end - start) / 1_000_000.0;
 
         System.out.printf("Time taken for the method %s from the class %s took %.4fms%n", methodName, className, timeTaken);
 
